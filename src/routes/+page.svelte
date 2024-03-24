0802771e5971e18bbfb5d91631f8671e339bfb94
James Pettit <etherealmachine@gmail.com>
2024-03-24 10:43:36 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  const imgs: string[] = [];
+  for (let i = 1; i <= 25; i++) {
+    imgs.push(`images/mollie${i}.jpg`);
+  }
+</script>
+
+<div>
+  {#each imgs as img}
+    <img class="w-[512px]" src={img} />
+  {/each}
+</div>
